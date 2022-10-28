a6ba24c550a17a26e83185d8c9f67470c091969b
Jeovanne Lugo <jeovanneclugo@gmail.com>
2022-10-29 07:33:04 +0800

    first commit

---
+++ modal/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int parentX, parentY;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form modalBackground = new Form();
+            using (modalForm modal = new modalForm()) {
+
+                modalBackground.StartPosition = FormStartPosition.Manual;
+                modalBackground.FormBorderStyle = FormBorderStyle.None;
+                modalBackground.Opacity = .50d;
+                modalBackground.BackColor = Color.Black;
+                modalBackground.Size = this.Size;
+                modalBackground.Location = this.Location;
+                modalBackground.ShowInTaskbar = false;
+                modalBackground.Show();
+                modal.Owner = modalBackground;
+
+                parentX = this.Location.X;
+                parentY = this.Location.Y;
+
+                modal.ShowDialog();
+                modalBackground.Dispose();
+
+
+            }
+        }
+    }
+}
---
+++ modal/modalForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modal
+{
+    public partial class modalForm : Form
+    {
+        public modalForm()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void modalEffect_Timer_Tick(object sender, EventArgs e)
+        {
+            if (Opacity >= 1)
+            {
+                modalEffect_Timer.Stop();
+            }
+            else {
+                Opacity += .02;
+            }
+
+            int y = Form1.parentY += 3; //INCREMENT
+            this.Location = new Point(Form1.parentX + 220,y) ;
+            if (y >= i) {
+                modalEffect_Timer.Stop();
+            }
+        }
+        int i;
+        private void modalForm_Load(object sender, EventArgs e)
+        {
+            i = Form1.parentY + 150;
+            this.Location = new Point(Form1.parentX + 220, Form1.parentY + 150);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
